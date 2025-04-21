437e1de406d23b707cb66251b78ea65858dccf8e
Muhammet Enes Çelikoğlu <86617575+Stingdev@users.noreply.github.com>
2025-04-21 14:36:23 +0300

    variables

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int hello_word = 5 ;
+        double num2= 5.4;
+        boolean b =true;
+        char c = '*' ; //tek bir karakter
+        String str = "dlksfmskfl"; //string
+        
+        
+        System.out.println(str);
+
+        
+    }
+}
